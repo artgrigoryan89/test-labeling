72d7f972cafff6a1538db203af2380359da26705
artgrigoryan <artgrigoryan500@gmail.com>
2018-09-01 17:57:33 +0400

    Last.

--- src/components/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import {formFieldReducer} from './module/form_module';
-
-const rootReducer = combineReducers({
-    data: formFieldReducer,
-});
-
-export default rootReducer;
